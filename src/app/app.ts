16d1a06dbc7b673d8f281a8aeeb59e532bc233a9
BeaMoura0906 <bea-moura@outlook.fr>
2025-09-28 22:36:20 +0200

    feat: toolbar

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,11 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { Assignments } from './assignments/assignments';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatButtonModule, MatIconModule, MatDividerModule, Assignments],
+  imports: [RouterOutlet, MatButtonModule, MatIconModule, MatDividerModule, Assignments, MatToolbarModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
