42e1ea32f7b9356b400f03efee1b501e271db446
BeaMoura0906 <bea-moura@outlook.fr>
2025-10-10 16:19:28 +0200

    style: assignment-details with mat-card

--- src/app/assignments/assignment-detail/assignment-detail.ts
+++ src/app/assignments/assignment-detail/assignment-detail.ts
@@ -1,14 +1,17 @@
-import { I } from '@angular/cdk/keycodes';
 import { Component, Input } from '@angular/core';
 import { Assignment } from '../assignment.model';
 import { DatePipe } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-assignment-detail',
-  imports: [DatePipe],
+  imports: [DatePipe, MatCardModule, MatButtonModule, MatCheckboxModule],
   templateUrl: './assignment-detail.html',
   styleUrl: './assignment-detail.css'
 })
+
 export class AssignmentDetail {
   @Input() assignmentTransmis?: Assignment;
 
